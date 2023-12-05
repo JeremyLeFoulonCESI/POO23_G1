ee2d719ea33e8b43d9f22c651438974117cc2f52
Jerem2360 <jlefoulon@hotmail.com>
2023-12-05 15:00:56 +0100

    Moved source code into .cpp files

--- DataAccessor.cpp
+++ DataAccessor.cpp
@@ -1,2 +1,61 @@
 #include "pch.h"
 #include "DataAccessor.h"
+
+
+String^ DataAccessor::insert(String^ table, String^ columns, ... array<Object^>^ values)
+{
+    String^ query = "INSERT INTO" + table + "(" + columns + ") VALUES ";
+    for each (Object ^ obj in values) {
+        String^ suiteQuery = obj->ToString();
+        query += "'" + suiteQuery + "', ";
+    };
+
+    if (values->Length > 0) {
+        query = query->Remove(query->Length - 2);
+    };
+
+    query += ";";
+    return query;
+}
+
+String^ DataAccessor::update(String^ table, array<String^>^ columns, int ID, String^ primaryKey, ... array<Object^>^ values)
+{
+    String^ query = "UPDATE" + table + "SET ";
+    for (int i = 0; i < columns->Length; i++) {
+        String^ name = columns[i];
+        Object^ value = values[i];
+        query += name + " = ";
+        query += "'" + value + "', ";
+    };
+
+    if (values->Length > 0) {
+        query = query->Remove(query->Length - 2);
+    };
+
+    query += "WHERE" + primaryKey + " = " + ID + ";";
+    return query;
+}
+
+String^ DataAccessor::select(String^ table, String^ criteria, ... array<String^>^ columns)
+{
+    String^ query = "SELECT (";
+    for each (String ^ name in columns) {
+        query += name + ", ";
+    };
+
+    if (columns->Length > 0) {
+        query = query->Remove(query->Length - 2);
+    };
+
+    query += ") ";
+    query += "FROM " + table + "WHERE " + criteria + ";";
+    return query;
+}
+
+String^ DataAccessor::remove(String^ table, String^ criteria)
+{
+    String^ query = "DELETE FROM ";
+    query += table + "WHERE " + criteria + ";";
+    return query;
+}
+
--- DataAccessor.h
+++ DataAccessor.h
@@ -7,62 +7,13 @@ using namespace MySql::Data::MySqlClient;
 ref class DataAccessor
 {
 public:
-    String^ insert(String^ table, String^ columns, ... array<Object^>^ values)
-    {
-        String^ query = "INSERT INTO" + table + "(" + columns + ") VALUES ";
-        for each (Object^ obj in values) {
-            String^ suiteQuery = obj->ToString();
-            query += "'" + suiteQuery + "', ";
-        };
+    String^ insert(String^ table, String^ columns, ... array<Object^>^ values);
 
-        if (values->Length > 0) {
-            query = query->Remove(query->Length - 2);
-        };
+    String^ update(String^ table, array<String^>^ columns, int ID, String^ primaryKey, ... array<Object^>^ values);
 
-        query += ";"; 
-        return query;
-    }
+    String^ select(String^ table, String^ criteria, ... array<String^>^ columns);
 
-    String^ update(String^ table, array<String^>^ columns, int ID,  String^ primaryKey, ... array<Object^>^ values)
-    {
-        String^ query = "UPDATE" + table + "SET ";
-        for (int i = 0; i < columns->Length; i++) {
-            String^ name = columns[i];
-            Object^ value = values[i];
-            query += name + " = ";
-            query += "'" + value + "', ";
-        };
-
-        if (values->Length > 0) {
-            query = query->Remove(query->Length - 2);
-        };
-
-        query += "WHERE" + primaryKey + " = " + ID + ";";
-        return query;
-    }
-
-    String^ select(String^ table, String^ criteria, ... array<String^>^ columns)
-    {
-        String^ query = "SELECT (";
-        for each (String^ name in columns) {
-            query += name + ", ";
-        };
-
-        if (columns->Length > 0) {
-            query = query->Remove(query->Length - 2);
-        };
-
-        query += ") ";
-        query += "FROM " + table + "WHERE " + criteria + ";";
-        return query;
-    }
-
-    String^ remove(String^ table, String^ criteria)
-    {
-        String^ query = "DELETE FROM ";
-        query += table + "WHERE " + criteria + ";";
-        return query;
-    }
+    String^ remove(String^ table, String^ criteria);
 };
 
 
