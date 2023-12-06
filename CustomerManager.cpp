2877d618e61f8b407468c32eeba7524c50b6a367
Jerem2360 <jlefoulon@hotmail.com>
2023-12-06 15:56:40 +0100

    started implementing getCustomer

--- CustomerManager.cpp
+++ CustomerManager.cpp
@@ -53,4 +53,58 @@ namespace Services {
 		data.id = id_customer;
 		return data;
 	}
+
+	CustomerData CustomerManager::getCustomer(int id) {
+
+		DataTable^ found = this->dbSearchRows(Components::Table::getAddressTable(),
+			gcnew array<String^> {"CodeClient_ClientFacture"},
+			gcnew array<Object^> {Int32(id)}
+		);
+
+		List<Address>^ delivery_addresses = gcnew List<Address>();
+		for each (DataRow^ row in found->Rows) {
+
+			int id_city = Convert::ToInt32(row->ItemArray[3]);
+
+			array<Object^>^ array_city = this->dbReadRow(Components::Table::getCityTable(), id_city);
+			
+			Address address = Address();
+			address.streetNum = Convert::ToString(row->ItemArray[1]);
+			address.streetName = Convert::ToString(row->ItemArray[2]);
+			address.cityName = Convert::ToString(array_city[1]);
+			address.cityCode = Convert::ToString(array_city[2]);
+
+			delivery_addresses->Add(address);
+		}
+
+		found = this->dbSearchRows(Components::Table::getAddressTable(),
+			gcnew array<String^> {"CodeClient_ClientLivre"},
+			gcnew array<Object^> {Int32(id)}
+		);
+
+		List<Address>^ invoice_addresses = gcnew List<Address>();
+		for each (DataRow ^ row in found->Rows) {
+
+			int id_city = Convert::ToInt32(row->ItemArray[3]);
+
+			array<Object^>^ array_city = this->dbReadRow(Components::Table::getCityTable(), id_city);
+
+			Address address = Address();
+			address.streetNum = Convert::ToString(row->ItemArray[1]);
+			address.streetName = Convert::ToString(row->ItemArray[2]);
+			address.cityName = Convert::ToString(array_city[1]);
+			address.cityCode = Convert::ToString(array_city[2]);
+
+			invoice_addresses->Add(address);
+		}
+
+		array<Object^>^ array_customer = this->dbReadRow(Components::Table::getCustomerTable(), id);
+
+		CustomerData result = CustomerData();
+		result.id = id;
+		result.lastName = Convert::ToString(array_customer[1]);
+		result.firstName = Convert::ToString(array_customer[2]);
+		result.birth = safe_cast<MySqlDateTime>(array_customer[3]);
+		result.email = Convert::ToString(array_customer[4]);
+	}
 }
