7d5740dafddd46b970a77c9417fb0d411b13a55e
Jerem2360 <jlefoulon@hotmail.com>
2023-12-06 09:03:46 +0100

    Added namespaces

--- DataAccessor.cpp
+++ DataAccessor.cpp
@@ -2,63 +2,68 @@
 #include "DataAccessor.h"
 
 
-String^ DataAccessor::insert(String^ table, String^ columns, ... array<Object^>^ values)
-{
-    String^ query = "INSERT INTO " + table + " (" + columns + ") VALUES (";
-    for each (Object ^ obj in values) {
-        String^ suiteQuery = obj->ToString();
-        query += "'" + suiteQuery + "', ";
-    };
+namespace Components {
 
-    if (values->Length > 0) {
-        query = query->Remove(query->Length - 2);
-    };
 
-    query += ");";
-    return query;
-}
+    String^ DataAccessor::insert(String^ table, String^ columns, ... array<Object^>^ values)
+    {
+        String^ query = "INSERT INTO " + table + " (" + columns + ") VALUES (";
+        for each (Object ^ obj in values) {
+            String^ suiteQuery = obj->ToString();
+            query += "'" + suiteQuery + "', ";
+        };
 
-String^ DataAccessor::update(String^ table, array<String^>^ columns, int ID, String^ primaryKey, ... array<Object^>^ values)
-{
-    String^ query = "UPDATE " + table + " SET ";
-    for (int i = 0; i < columns->Length; i++) {
-        String^ name = columns[i];
-        Object^ value = values[i];
-        query += name + " = ";
-        query += "'" + value + "', ";
-    };
+        if (values->Length > 0) {
+            query = query->Remove(query->Length - 2);
+        };
 
-    if (values->Length > 0) {
-        query = query->Remove(query->Length - 2);
-    };
+        query += ");";
+        return query;
+    }
 
-    query += " WHERE " + primaryKey + " = " + ID + ";";
-    return query;
-}
+    String^ DataAccessor::update(String^ table, array<String^>^ columns, int ID, String^ primaryKey, ... array<Object^>^ values)
+    {
+        String^ query = "UPDATE " + table + " SET ";
+        for (int i = 0; i < columns->Length; i++) {
+            String^ name = columns[i];
+            Object^ value = values[i];
+            query += name + " = ";
+            query += "'" + value + "', ";
+        };
 
-String^ DataAccessor::select(String^ table, String^ criteria, ... array<String^>^ columns)
-{
-    String^ query = "SELECT (";
-    for each (String ^ name in columns) {
-        query += name + ", ";
-    };
+        if (values->Length > 0) {
+            query = query->Remove(query->Length - 2);
+        };
 
-    if (columns->Length > 0) {
-        query = query->Remove(query->Length - 2);
-    };
+        query += " WHERE " + primaryKey + " = " + ID + ";";
+        return query;
+    }
 
-    query += ") ";
-    query += "FROM " + table;
-    if (criteria->Length)
-        query += " WHERE " + criteria;
-    query += ";";
-    return query;
-}
+    String^ DataAccessor::select(String^ table, String^ criteria, ... array<String^>^ columns)
+    {
+        String^ query = "SELECT (";
+        for each (String ^ name in columns) {
+            query += name + ", ";
+        };
 
-String^ DataAccessor::remove(String^ table, String^ criteria)
-{
-    String^ query = "DELETE FROM ";
-    query += table + " WHERE " + criteria + ";";
-    return query;
+        if (columns->Length > 0) {
+            query = query->Remove(query->Length - 2);
+        };
+
+        query += ") ";
+        query += "FROM " + table;
+        if (criteria->Length)
+            query += " WHERE " + criteria;
+        query += ";";
+        return query;
+    }
+
+    String^ DataAccessor::remove(String^ table, String^ criteria)
+    {
+        String^ query = "DELETE FROM ";
+        query += table + " WHERE " + criteria + ";";
+        return query;
+    }
+    
 }
 
--- DataAccessor.h
+++ DataAccessor.h
@@ -1,19 +1,23 @@
 #pragma once
 #include <iostream>
 
-using namespace System;
-using namespace MySql::Data::MySqlClient; 
+namespace Components {
 
-ref class DataAccessor
-{
-public:
-    static String^ insert(String^ table, String^ columns, ... array<Object^>^ values);
+    using namespace System;
+    using namespace MySql::Data::MySqlClient;
 
-    static String^ update(String^ table, array<String^>^ columns, int ID, String^ primaryKey, ... array<Object^>^ values);
+    ref class DataAccessor
+    {
+    public:
+        static String^ insert(String^ table, String^ columns, ... array<Object^>^ values);
 
-    static String^ select(String^ table, String^ criteria, ... array<String^>^ columns);
+        static String^ update(String^ table, array<String^>^ columns, int ID, String^ primaryKey, ... array<Object^>^ values);
 
-    static String^ remove(String^ table, String^ criteria);
-};
+        static String^ select(String^ table, String^ criteria, ... array<String^>^ columns);
+
+        static String^ remove(String^ table, String^ criteria);
+    };
+
+}
 
 
