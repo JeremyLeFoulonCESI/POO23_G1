6c6b140f0d9ec7012dc065bf61094d566325574d
Jerem2360 <jlefoulon@hotmail.com>
2023-12-06 10:31:40 +0100

    Implemented instance methods of class 'Table'

--- Table.cpp
+++ Table.cpp
@@ -3,6 +3,72 @@
 
 
 namespace Components {
+	Table::Table(String^ name, array<String^>^ primary_keys, array<String^>^ keys) :
+		name(name), primary_keys(primary_keys), keys(keys)
+	{}
 
+	String^ Table::bindKeysForUpdate(... array<Object^>^ values) {
+		String^ result = "";
+
+		if (values->Length < this->keys->Length) {
+			return result;
+		}
+		result += "(";
+
+		for (int i = 0; i < this->keys->Length; i++) {
+			String^ key = this->keys[i];
+			Object^ value = values[i];
+			result += key + " = " + value + ",";
+		}
+
+		if (keys->Length > 0 && result->EndsWith(",")) {
+			result = result->Remove(result->Length - 2);
+		}
+
+		result += ")";
+		return result;
+	}
+
+	String^ Table::bindPrimaryKeysForUpdate(... array<Object^>^ values) {
+		String^ result = "";
+
+		if (values->Length < this->primary_keys->Length) {
+			return result;
+		}
+		result += "(";
+
+		for (int i = 0; i < this->primary_keys->Length; i++) {
+			String^ key = this->primary_keys[i];
+			Object^ value = values[i];
+			result += key + " = " + value + ",";
+		}
+
+		if (result->Length > 0 && result->EndsWith(",")) {
+			result = result->Remove(result->Length - 2);
+		}
+
+		result += ")";
+		return result;
+	}
+	String^ Table::bundleForInsert() {
+		String^ result = "(";
+		for each (String ^ key in this->keys) {
+			result += key + ",";
+		}
+		if (result->Length > 0 && result->EndsWith(",")) {
+			result = result->Remove(result->Length - 2);
+		}
+		result += ")";
+		return result;
+	}
+	array<String^>^ Table::getPrimaryKeys() {
+		return this->primary_keys;
+	}
+	array<String^>^ Table::getKeys() {
+		return this->keys;
+	}
+	String^ Table::getName() {
+		return this->name;
+	}
 }
 
